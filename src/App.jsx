a2674509df11e714224659d9fa13a77ea7a57589
Ashish Sengar <65999078+Ashishss21@users.noreply.github.com>
2024-02-29 20:28:55 +0530

    Added BrowserRouter and Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-function App() {
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components/index";
 
+const App = () => {
   return (
-    <div>
-      Ashish Sengar Portfolio
-    </div>
-    )
-}
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero/>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className="relative z-0">
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
